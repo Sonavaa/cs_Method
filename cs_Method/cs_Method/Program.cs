bbeac1a974ba70b7254ffbcafadab32081e596cb
Sonava <sonava@code.edu.az>
2023-10-09 22:30:27 +0400

    Task tamamlandı

---
+++ cs_Method/cs_Method/Program.cs
@@ -0,0 +1,29 @@
+﻿//int tipindən array və 1 int tipindən ədəd qəbul edən metod yazın.
+//Metod bu ədədin bu arrayin hər hansı bir elementinə bərabər olub-olmadığını tapsın.
+//Beraber olarsa ekrana true cixarin olmasa false.
+
+//using System.ComponentModel.Design;
+
+//int[] Nums = { 3, 5, 6, 3, 24, 47, 56, 85 };
+//string num = Console.ReadLine();
+//int Num = int.Parse(num);
+//int Count = 0;
+
+//bool IsEqual(int[] Arr, int Num)
+//{
+//    for (int i = 0; i < Nums.Length; i++)
+//    {
+//        if (Nums[i] != Num)
+//        {
+//            Count++;
+//        }
+//    }
+//    if (Count == Nums.Length)
+//    {
+//        Console.WriteLine("False");
+//    }
+//    else
+//        Console.WriteLine("True");
+//    return true;
+//}
+//IsEqual(Nums,Num);
